5d270936ac12b2bf5e55ad87e6e3692fa612bd5e
Vladimir Greshilov <greshilov@a2design.biz>
2017-08-08 12:17:09 +0600

    Wrapped parsing into try catch, refactored tg sending

--- background.js
+++ background.js
@@ -78,7 +78,6 @@ function onStartup() {
         } else {
             initStorage();
         }
-        getTelegramChatId();
         setCommentatorIcon();
         getUpdates();
     });
--- javascripts/options.js
+++ javascripts/options.js
@@ -26,8 +26,6 @@ function save() {
         'targets'               : localStorage.targets,
         'posts'                 : localStorage.posts
     }, function() {});
-
-    getTelegramChatId();
 }
 
 
